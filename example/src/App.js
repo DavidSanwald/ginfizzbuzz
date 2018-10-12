17d70799302eb6b946e1aca937f4859b1b499351
DavidSanwald <david.sanwald@gmail.com>
2018-10-12 02:16:18 +0200

    yay

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import GinFizzBuzzer from 'ginfizzbuzzer'
+import './App.css';
+import { Transition, animated } from 'react-spring'
 
-import ExampleComponent from 'ginfizzbuzzer'
+const fns = {
+  Fizz: style => <animated.div key={'Fizz'} className="Word" style={{ ...style, background: '#5DA2D5' }} >Fizz</animated.div>,
+  FizzBuzz: style => <animated.div key={'FizzBuzz'} className="Word" style={{ ...style, background: '#F78888' }} ><p>Fizz</p><p>Buzz</p></animated.div >,
+  Buzz: style => <animated.div key={'Buzz'} className="Word" style={{ ...style, background: '#F3D250' }} >Buzz</animated.div>
+}
 
-export default class App extends Component {
+
+const page = element => fns[element] || (style => <animated.div key={element} className="Word" style={{ ...style, background: '#ECECEC' }} >{element.toString()}</animated.div>)
+
+class App extends Component {
+  onAppClick = (e) => console.log('Still Working')
+
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
+      <div className="App">
+        <GinFizzBuzzer >
+          {({ count, getToggleProps, reset, Fizz, Buzz, toggle, isRunning, FizzBuzz }) => {
+            const element = Fizz || Buzz || FizzBuzz || count
+            const el = page(element)
+            return (
+              <div {...getToggleProps({ onClick: this.onAppClick })} style={{ cursor: 'pointer', height: '100%', display: 'flex', flexDirection: 'column' }}>
+                {isRunning ? <Transition
+                  native
+                  from={{ opacity: 0, transform: 'translate3d(100%,0,0)' }}
+                  enter={{ opacity: 1, transform: 'translate3d(0%,0,0)' }}
+                  leave={{ opacity: 0, transform: 'translate3d(-50%,0,0)' }}>
+                  {el}
+                </Transition> :
+                  <Transition from={{ opacity: 0 }} enter={{ opacity: 1 }} leave={{ opacity: 0 }}>
+                    {style => < animated.div key={'Buzz'} className="Word" style={{ ...style, color: 'black', background: '#F78888' }} >Stop</animated.div>}
+                  </Transition>}
+              </div>
+            )
+          }
+          }
+        </GinFizzBuzzer>
+      </div >
+    );
   }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,104 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+import React from 'react';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+
+const defaultProps = {
+  interval: 1000,
+  start: 0,
+  initialRunning: true,
+  onReset: () => { }
+}
+
+const isFizz = n => n % 3 === 0
+const isBuzz = n => n % 5 === 0
+const both = (fn1, fn2) => n => fn1(n) && fn2(n)
+const isFizzBuzz = both(isFizz, isBuzz)
+
+const inc = x => x + 1
+const incCount = previousState => ({
+  ...previousState, count: inc(previousState.count)
+})
+
+const callAll = (...fns) => (...args) =>
+  fns.forEach(fn => fn && fn(...args))
+
+class FizzBuzzer extends React.Component {
+
+  initialState = { count: 0, isRunning: this.props.initialRunning }
+
+
+
+  state = { ...this.initialState }
+
+  _clearIntervalID = () => {
+    if (this.state.intervalID) {
+      this.intervalID = clearInterval(this.state.intervalID)
+    }
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  _setIntervalID = () => {
+    if (!this.intervalID) {
+      this.intervalID = setInterval(
+        () => this.tick(),
+        this.props.interval
+      )
+    }
+  }
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+  componentDidMount () {
+    this.state.isRunning ? this._setIntervalID() : this._clearIntervalID()
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    !prevState.isRunning ? this._setIntervalID() : this._clearIntervalID()
+  }
+
+  componentWillUnmount () {
+    this._clearIntervalID()
+  }
+
+  toggle = () => {
+    this.setState(({ isRunning }) => ({ isRunning: !isRunning }))
+  }
+
+  start = () => {
+    this.setState({ isRunning: true })
+  }
+
+  reset = () => {
+    this.setState(this.initialState)
+  }
+
+  stop = () => {
+    this.setState({ isRunning: false })
+  }
+
+  getToggleProps = ({ onClick, ...props } = {}) => ({ onClick: callAll(onClick, this.toggle), ...props })
+
+  getStateAndHelpers () {
+    return {
+      Fizz: isFizz(this.state.count) && !isFizzBuzz(this.state.count) ? 'Fizz' : false,
+      Buzz: isBuzz(this.state.count) && !isFizzBuzz(this.state.count) ? 'Buzz' : false,
+      FizzBuzz: isFizzBuzz(this.state.count) ? 'FizzBuzz' : false,
+      count: this.state.count,
+      isRunning: this.state.isRunning,
+      toggle: this.toggle,
+      reset: this.reset,
+      getToggleProps: this.getToggleProps
+    }
+  }
+
+  tick () {
+    this.setState(
+      incCount
+    );
+  }
+
+  render () {
+    return this.props.children(this.getStateAndHelpers());
   }
 }
+
+FizzBuzzer.defaultProps = defaultProps
+
+export default FizzBuzzer
